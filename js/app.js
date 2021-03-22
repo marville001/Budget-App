e5f4bad6d352da8e0a1e4b92dc616e165d6109ee
marville001 <mwangimartin1904@gmail.com>
2021-03-22 16:11:52 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -72,6 +72,7 @@ class UI {
       this.itemID++;
       this.itemList.push(expense);
       this.addExpense(expense);
+      this.showBalance();
     }
   }
   //add expense
@@ -93,12 +94,20 @@ class UI {
        </a>
       </div>
      </div>`;
-     this.expenseList.appendChild(div);
+    this.expenseList.appendChild(div);
   }
 
   //total expense
   totalExpense() {
-    let total = 400;
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce((acc, curr) => {
+        acc += curr.amount;
+        return acc;
+      }, 0);
+
+    }
+    this.expenseAmount.textContent = total;
     return total;
   }
 }
